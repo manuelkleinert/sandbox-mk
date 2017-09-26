584985bd53c2244039e0c3fcd667768d7a0ed4ba
Manuel Kleinert <info@xpager.ch>
2017-09-26 10:25:31 +0200

    remove

--- assets/js/utils/page-loader.js
+++
@@ -1,6 +0,0 @@
-(function preLoader() {
-  $(window).on('load', () => {
-    const loader = $('.page-loader');
-    UIkit.util.animate(loader, 'uk-animation-fade', 400, false, true).then(() => loader.remove());
-  });
-}(jQuery, UIkit));
--- var/config/extensions.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-return [
-    "bundle" => [
-        "WvisionBundle\\WvisionBundle" => TRUE
-    ]
-];
